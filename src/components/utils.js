63c98d82f227afc2aa0051a92ce76031efe8cbe6
SachaBogaers <san_bogaers@hotmail.com>
2021-04-13 23:40:45 +0200

    Adding utility functions

--- src/components/utils.js
+++ src/components/utils.js
@@ -3,5 +3,31 @@ function onlyUnique(value, index, self) {
 }
 
 const getNames = (data) => data.map(person => person.name).filter(onlyUnique);
+const getExercises = (data) => data.map(item => item.exercise).filter(onlyUnique);
 
-export { onlyUnique, getNames }
+function pickRandom(list) {
+	return list[Math.floor(Math.random() * list.length)];
+}
+
+const generateEmail = (firstName, lastName) => {
+	console.log("generating email", firstName, lastName)
+	firstName = firstName.toLowerCase()
+	lastName = lastName.toLowerCase()
+	const providers = ['hotmail.com', 'hotmail.nl', 'gmail.com', 'live.com', 'mail.com', 'outlook.com', 'yahoo.com']
+	const formats = [`${firstName}.${lastName}`, `${firstName.charAt(0)}.${lastName}`, `${firstName}_${lastName}`, `${firstName}${lastName}`]
+	const format = pickRandom(formats);
+	const provider = pickRandom(providers);
+	const email = `${format}@${provider}`
+	return email
+}
+
+const randomNumber = (min, max) => { 
+    return Math.floor(Math.random() * (max - min) + min);
+} 
+
+const generatePhoneNumber = () => {
+	const number = `06-${Math.floor(Math.random() * 89999999 + 100000)}`
+	return number
+}
+
+export { onlyUnique, getNames, getExercises, pickRandom, generateEmail, generatePhoneNumber, randomNumber }
