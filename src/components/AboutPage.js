e8a16f7b67f9b7759ed5c409bc0857a79da6e951
SachaBogaers <san_bogaers@hotmail.com>
2021-04-17 19:01:41 +0200

    Adding link on about page

--- src/components/AboutPage.js
+++ src/components/AboutPage.js
@@ -2,7 +2,10 @@ function AboutPage() {
 	return (
 		<main className="AboutPage">
 			<h1>About</h1>
-			<p className="aboutText">Hi! Thanks for taking a look at this student dashboard project. It was made as an assignment for the Front-End Developer course by Winc Academy. Do you want to see more of my projects? Please take a look at my <a href="https://github.com/SachaBogaers">Github page</a>.</p>
+			<p className="aboutText">Hi! Thanks for taking a look at this student dashboard project.
+			It was made as an assignment for the Front-End Developer course by <a href="https://www.wincacademy.nl/opleidingen/front-end-development" target='_blank'>Winc Academy</a>.
+			Do you want to see more of my projects?
+			Please take a look at my <a href="https://github.com/SachaBogaers" target='_blank'>Github page</a>.</p>
 		</main>
 
 	);
