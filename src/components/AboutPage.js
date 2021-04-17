654d05efa030185201727afb3163edb4739dfe05
SachaBogaers <san_bogaers@hotmail.com>
2021-04-17 10:59:15 +0200

    Adding text about page

--- src/components/AboutPage.js
+++ src/components/AboutPage.js
@@ -2,7 +2,7 @@ function AboutPage() {
 	return (
 		<main className="AboutPage">
 			<h1>About</h1>
-			<h2>This is the about page</h2>
+			<p className="aboutText">Hi! Thanks for taking a look at this student dashboard project. It was made as an assignment for the Front-End Developer course by Winc Academy. Do you want to see more of my projects? Please take a look at my <a href="https://github.com/SachaBogaers">Github page</a>.</p>
 		</main>
 
 	);
